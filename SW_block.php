202e9191a3f9126a066e7a2cdc5a13b572685eb0
Sebastian Wesołowski <sebastian@artnova.com.pl>
2019-04-16 21:36:51 +0200

    initil sample structure for plugin

---
+++ SW_block.php
@@ -0,0 +1,41 @@
+<?php
+/** 
+* Plugin Name:      SW_block
+* Plugin URI:       https://github.com/SebastianWesolowski/SW_block
+* Description:      Gutenber block for bhp page
+* Author:           Sebastian Wesołowski
+* Author URI:       warsztatkodu.pl
+* Text Domain:      SW_block
+* Domain Path:      /resources/lang
+* Version:          0.2
+*
+* @package         SW_block
+*/
+
+wp_register_script(
+	'SW_block-js',
+	plugin_dir_url(__FILE__) .'dist/block.js', 
+	[
+		'wp-element',
+		'wp-i18n',
+		'wp-blocks',
+	],
+	'',
+	null,
+	true
+);
+
+wp_register_style(
+    'SW_block-public-css',
+    plugin_dir_url(__FILE__) .'dist/block.css',
+    [],
+    null
+);
+
+register_block_type(
+    'SW/block', [
+        'editor_script' => 'SW_block-js',
+        'editor_style'  => 'SW_block-public-css',
+        'style'         => 'SW_block-public-css',
+    ]
+);
