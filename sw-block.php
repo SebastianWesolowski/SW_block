d73ed26f6e04d4127af501c434446fe27d7b21ca
Sebastian Wesołowski <Sebastian.wesolowski.sw@gmail.com>
2019-04-21 18:01:52 +0200

    ADD RULE TO PHPCS

--- sw-block.php
+++ sw-block.php
@@ -1,67 +1,52 @@
 <?php
-
 /**
- * Plugin Name:      sw-block
- * Plugin URI:       https://github.com/sebastianwesolowski/sw-block
- * Description:      Gutenber block for bhp page
- * Version:          0.2
- * Author:           Sebastian Wesołowski
- * Author URI:       warsztatkodu.pl, wesolowski.dev
- * Text Domain:      sw-block
- * Domain Path:      /resources/lang
- * License:        	 MIT License
- * License URI:      http://opensource.org/licenses/MIT
+ * Block gutenberg
  *
- * @package         sw-block
+ * @package     sw-block
+ * @author      Sebastian Wesołowski
+ * @license     MIT License
+ *
+ * Plugin Name: sw-block
+ * Plugin URI:  https://github.com/sebastianwesolowski/sw-block
+ * Description: Gutenber block for bhp page
+ * Version:     0.2
+ * Author:      Sebastian Wesołowski
+ * Author URI:  http://warsztatkodu.pl, http://wesolowski.dev
+ * Text Domain: sw-block
+ * Domain Path: /resources/lang
+ * License:     MIT License
+ * License URI: http://opensource.org/licenses/MIT
  */
 
-// wp_register_script(
-// 	'sw-block-js',
-// 	plugin_dir_url(__FILE__) .'dist/block.js', 
-// 	[
-// 		'wp-element',
-// 		'wp-i18n',
-// 		'wp-blocks',
-// 	],
-// 	'',
-// 	null,
-// 	true
-// );
-// wp_register_style('sw-block-public-css', plugin_dir_url(__FILE__) . 'dist/block.css', array(), null);
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+  exit;
+}
+// Require once the Composer Autoload.
+if ( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
+  require_once dirname( __FILE__ ) . '/vendor/autoload.php';
+}
+/**
+ * The code that runs during plugin activation
+ */
+function activate_gutenberg_blocks() {
+  Inc\Base\Activate::activate();
+}
 
-// register_block_type('SW/block', array(
-//     'editor_script' => 'sw-block-js',
-//     'editor_style' => 'sw-block-public-css',
-//     'style' => 'sw-block-public-css'
-// ));
+register_activation_hook( __FILE__, 'activate_gutenberg_blocks' );
+/**
+ * The code that runs during plugin deactivation
+ */
+function deactivate_gutenberg_blocks() {
+  Inc\Base\Deactivate::deactivate();
+}
 
+register_deactivation_hook( __FILE__, 'deactivate_gutenberg_blocks' );
+/**
+ * Initialize all the core classes of the plugin
+ */
+if ( class_exists( 'Inc\\Init' ) ) {
+  Inc\Init::register();
+}
 
-add_action(
-    'init', function () {
-        wp_register_script(
-            'sw-block-js',
-            plugin_dir_url(__FILE__) .'dist/block.js',
-            [
-                'wp-element',
-                'wp-i18n',
-                'wp-blocks',
-            ],
-            '',
-            null,
-            true
-        );
-        wp_register_style(
-            'sw-block-public-css',
-            plugin_dir_url(__FILE__) .'dist/block.css',
-            [],
-            null
-        );
-        register_block_type(
-            'tinyblocks/markdown', [
-                'editor_script' => 'sw-block-js',
-                'editor_style'  => 'sw-block-public-css',
-                'style'         => 'sw-block-public-css',
-            ]
-        );
-    }
-);
+wp_register_script( 'demo-block-js', plugin_dir_url( __FILE__ ) . 'dist/block.js', ['wp-element', 'wp-i18n', 'wp-blocks'], '', null, true );
